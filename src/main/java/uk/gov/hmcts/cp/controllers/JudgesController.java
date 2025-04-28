e7f1acc947e6d164437cdafcae440a65e09ac715
Duncan Crawford <duncan@noughtsandones.co.uk>
2025-04-28 17:53:11 +0100

    fix OWSAP warning

--- src/main/java/uk/gov/hmcts/cp/controllers/JudgesController.java
+++ src/main/java/uk/gov/hmcts/cp/controllers/JudgesController.java
@@ -9,6 +9,8 @@ import org.slf4j.LoggerFactory;
 import uk.gov.hmcts.cp.openapi.api.JudgesApi;
 import uk.gov.hmcts.cp.openapi.model.Judges;
 import uk.gov.hmcts.cp.services.JudgesService;
+import org.apache.commons.lang3.StringUtils;
+import org.owasp.encoder.Encode;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,9 +22,14 @@ public class JudgesController implements JudgesApi {
 
     @Override
     public ResponseEntity<Judges> getJudgeById(String courtId) {
-        log.info("Received request to fetch judge details for courtId: {}", courtId);
+        if (StringUtils.isEmpty(courtId)) {
+            log.warn("No court identifier provided: {}", HttpStatus.BAD_REQUEST);
+            return ResponseEntity.badRequest().build();
+        }
+        String safeCourtId = Encode.forHtml(courtId);
+        log.debug("Received request to fetch judge details for courtId: {}", safeCourtId);
         Judges judges = judgesService.getJudge(courtId);
-        log.debug("Successfully fetched judge details for courtId: {}", courtId);
+        log.debug("Successfully fetched judge details for courtId: {}", safeCourtId);
         return new ResponseEntity<>(judges, HttpStatus.OK);
     }
 
