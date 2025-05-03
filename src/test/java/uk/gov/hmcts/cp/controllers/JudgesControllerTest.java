f7394c770295c44ea441fa559787eb015574ba33
Duncan Crawford <duncan@noughtsandones.co.uk>
2025-05-03 06:02:40 +0100

    add cyclonedxBom, dependabot and some minor build tweaks

--- src/test/java/uk/gov/hmcts/cp/controllers/JudgesControllerTest.java
+++ src/test/java/uk/gov/hmcts/cp/controllers/JudgesControllerTest.java
@@ -2,10 +2,6 @@ package uk.gov.hmcts.cp.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
