349bf1b5ce386c7aeb6ca827ebc245805b087ce5
Duncan Crawford <duncan.crawford@hmcts.net>
2025-07-10 17:28:05 +0100

    refactor the build to ensure the provider contract tests are executed… (#131)
    * refactor the build to ensure the provider contract tests are executed within the pipeline

--- src/pactVerificationTest/java/uk/gov/hmcts/cp/pact/provider/JudgesProviderPactTest.java
+++ src/pactVerificationTest/java/uk/gov/hmcts/cp/pact/provider/JudgesProviderPactTest.java
@@ -11,27 +11,33 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.cp.openapi.model.Judges;
-import uk.gov.hmcts.cp.repository.JudgesRepository;
 import uk.gov.hmcts.cp.pact.helper.JsonFileToObject;
+import uk.gov.hmcts.cp.repository.JudgesRepository;
 
 import java.util.UUID;
+
 import static java.util.UUID.fromString;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ExtendWith({SpringExtension.class, PactVerificationInvocationContextProvider.class})
 @Provider("CPRefDataJudgesProvider")
 @PactBroker(
-        scheme = "https",
-        host = "${pact.broker.host}",
+        url = "${pact.broker.url}",
         authentication = @PactBrokerAuth(token = "${pact.broker.token}")
 )
 @Tag("pact")
 public class JudgesProviderPactTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(JudgesProviderPactTest.class);
+
+    private static final UUID STUBBED_JUDGE_ID = fromString("a228cbdb-e1d0-4d29-bb44-06b7669b66a3");
 
     @Autowired
     private JudgesRepository judgesRepository;
@@ -41,17 +47,17 @@ public class JudgesProviderPactTest {
 
     @BeforeEach
     void setupTarget(PactVerificationContext context) {
-        System.out.println("Running test on port: " + port);
+        LOG.atDebug().log("Running test on port: " + port);
         context.setTarget(new HttpTestTarget("localhost", port));
-        System.out.println("pact.verifier.publishResults: " + System.getProperty("pact.verifier.publishResults"));
+        LOG.atDebug().log("pact.verifier.publishResults: " + System.getProperty("pact.verifier.publishResults"));
     }
 
     @State("judge exists")
     public void setUpJudges() throws Exception {
         judgesRepository.clearAll();
-        final UUID judgeId = fromString("a228cbdb-e1d0-4d29-bb44-06b7669b66a3");
+        LOG.atDebug().log("Setting up judges for judgeId: {}", STUBBED_JUDGE_ID);
         Judges judges = JsonFileToObject.readJsonFromResources("judges.json", Judges.class);
-        judgesRepository.saveJudges(judgeId, judges);
+        judgesRepository.saveJudges(STUBBED_JUDGE_ID, judges);
     }
 
     @TestTemplate
